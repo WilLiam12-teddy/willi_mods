ca636b5058b9754b2e245b02da2cc8547c2ceeae
WilLiam12-teddy <willteddydoze@gmail.com>
2020-06-04 05:25:21 -0300

    Initial Commit

---
+++ willi_block/init.lua
@@ -0,0 +1,27 @@
+minetest.register_node("willi_block:blockw", {
+	description = "Willi Block",
+	tiles = {"willi_block.png"},
+	is_ground_content = false,
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("willi_block:blockr", {
+	description = "Ruan Block",
+	tiles = {"ruan_block.png"},
+	is_ground_content = false,
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("willi_block:blockc", {
+	description = "Caio Block",
+	tiles = {"caio_block.png"},
+	is_ground_content = false,
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("willi_block:blocka", {
+	description = "Alana Block",
+	tiles = {"alana_block.png"},
+	is_ground_content = false,
+	groups = {cracky=3, stone=1}
+})
---
+++ willi_tool/init.lua
@@ -0,0 +1,4 @@
+minetest.register_craftitem("willi_tool:will", {
+	description = "Meu proprio tool",
+	inventory_image = "willi_tool.png"
+})
